2947488b761a274eadb02a7b5f7ee51d7d6c71cb
adigwe <dadyjosh@gmail.com>
2017-07-08 20:50:34 +0100

    Final Submission

--- stats_.c
+++ stats_.c
@@ -9,13 +9,14 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
+ * @brief Calculates and prints statistics data for an array of numbers
  *
- * <Add Extended Description Here>
+ * This function calculates and prints statistics data, of an array of given numbers,   
+ * to the screen
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Ikechukwu Adigwe
+ * @date 4th July 2017
  *
  */
 
@@ -31,13 +32,106 @@ void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
+                              200, 122, 150,  90,  92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  print_array(SIZE, test);
+  
+  sort_array(SIZE, test);
+  
+  find_minimum(SIZE, test);
+  
+  find_maximum(SIZE, test);
+
+  
+  printf("\nMean = %f\n",find_mean(SIZE, test));
+
+  
 
 }
 
 /* Add other Implementation File Code Here */
+
+  void print_array(int a, unsigned char b[]){
+
+   int i;
+
+    for (i = 0; i < a; i++){
+      printf("\ntest[%d] = %d", i, b[i]);
+    }
+  }
+
+
+  void sort_array(int n, unsigned char number[]){
+
+   int i, a, j;
+
+  for (i = 0; i < n; ++i)
+    {
+        for (j = i + 1; j < n; ++j)
+        {
+            if (number[i] < number[j])
+            {
+                a = number[i];
+                number[i] = number[j];
+                number[j] = a;
+            }
+        }
+    }
+    printf("\n\nThe numbers arranged in descending order are given below\n");
+    for (i = 0; i < n; ++i)
+    {
+        printf("%d\n", number[i]);
+    }
+ }
+
+
+  void find_minimum(int c, unsigned char d[]){
+
+  unsigned char smallest;
+  int i;
+
+/*Consider first element as smallest */
+   smallest = d[0];
+  
+  
+   for (i = 0; i < c; i++) {
+      if (d[i] < smallest) {
+         smallest = d[i];
+      }
+   }
+ 
+   /* Print out the Result */
+   printf("\nSmallest Element : %d", smallest);
+
+  }
+
+
+  void find_maximum(int e, unsigned char f[]){
+
+  unsigned char largest;
+  int i;
+
+/*Consider first element as largest */
+   largest = f[0];
+   
+   for (i = 0; i < e; i++) {
+      if (f[i] > largest) {
+         largest = f[i];
+      }
+   }
+ 
+   /* Print out the Result */
+   printf("\nLargest Element : %d", largest);
+
+  }
+
+  float find_mean(int m, unsigned char p[]) {
+    int sum=0, i;
+    for(i=0; i<m; i++)
+        sum+=p[i];
+    return((float)sum/m);
+  }
+
+
--- stats_.h
+++ stats_.h
@@ -9,13 +9,20 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file   stats.h
+ * @brief Includes function declarations of functions in stats.c
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * This library includes function declarations for the following functions:
+ * print_statistcs();
+ * print_array();
+ * find_median();
+ * find_mean();
+ * find_maximum();
+ * find_minimum();
+ * sort_array();
+ * 
+ * @author Ikechukwu Adigwe
+ * @date 10th May 2017
  *
  */
 #ifndef __STATS_H__
@@ -24,9 +31,9 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief print_statistcs() prints the statistics of an array including minimum,    * maximum, mean, and median
  *
- * <Add Extended Description Here>
+ * The function takes two(2) input parameters - An unsigned char pointer to an n- * element data array,and an unsigned integer as the size of the array - and returns  * an unsigned char result from the function
  *
  * @param <Add InputName> <add description here>
  * @param <Add InputName> <add description here>
@@ -36,5 +43,14 @@
  * @return <Add Return Informaiton here>
  */
 
+  void print_array(int, unsigned char[]);
+
+  void sort_array(int, unsigned char[]);
+  
+  void find_minimum(int, unsigned char[]);
+  
+  void find_maximum(int, unsigned char[]);
+
+  float find_mean(int, unsigned char[]);
 
 #endif /* __STATS_H__ */
